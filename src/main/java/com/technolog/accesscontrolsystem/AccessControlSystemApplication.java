88ea1943274228c0f85e0f40b4b388c8f241c62b
AnSoldatkin <anton.soldatkin@odysseusinc.com>
2020-02-22 20:55:48 +0300

    initial commit

---
+++ src/main/java/com/technolog/accesscontrolsystem/AccessControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.technolog.accesscontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technolog/accesscontrolsystem/AccessControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technolog.accesscontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
